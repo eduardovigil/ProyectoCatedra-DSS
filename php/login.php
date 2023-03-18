2ebc92d7a9fe567d7834326cbe4152dea401c9de
leandro <100099447+carlosLGM@users.noreply.github.com>
2023-03-17 23:13:58 -0600

    update login
    cambios de gestores

--- php/login.php
+++ php/login.php
@@ -13,12 +13,49 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $result = $stmt->get_result();
     $user = $result->fetch_assoc();
     $pass =$_POST['password'];
+
+
+    
     // Verificar si el usuario existe y si la contraseña es correcta
     if ($user && password_verify($pass, $user['password'])) {
         // Iniciar sesión y redirigir al usuario a la página de inicio
         $_SESSION['usuario'] = $user; 
+        $rol = $user['idroles'];
+
+
+        switch ($rol) {
+            case 14:
+                header('Location: ../html/GerenteSucursal.php');
+                break;
+            case 4:
+                header('Location: cajero.php');
+                break;
+            case 24:
+                header('Location: GerenteGeneral.php');
+                break;
+            case 34:
+                header('Location: Cliente.php');
+                break;
+            case 44:
+                header('Location: Dependiente.php');
+                break;
+            case 54:
+                header('Location: PersonalLimpieza.php');
+                break;
+            case 64:
+                header('Location: PersonalMesa.php');
+            break;
+            case 74:
+                header('Location: Secretaria.php');
+                break;
+            case 84:
+                header('Location:PersonalSeguridad.php');
+                break;
+            default:
+                echo " No existe rol";
+                break;
+        }
         
-        header('Location: ../html/dashboard.php');
         exit();
     } else {
         // Mostrar un mensaje de error
